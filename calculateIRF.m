7ce43233e14fba7cddf3c81442147a70ff9769b6
Chu Chen <CreLox@users.noreply.github.com>
2020-01-17 15:31:06 -0500

    Add files via upload

--- calculateIRF.m
+++ calculateIRF.m
@@ -1,28 +1,32 @@
-% calculateIRF(EarlyPulseOrLatePulse, TimeWindow)
-function calculateIRF(EarlyPulseOrLatePulse, TimeWindow)
+% calculateIRF(EarlyPulseOrLatePulse, MicroTimeRange)
+function calculateIRF(EarlyPulseOrLatePulse, MicroTimeRange)
     %% Read the header and micro-time data
     [Trace, Filename, FCSHeader] = readMicroTime;
     
     %% Determine parameters
     ADCResolution = double(FCSHeader.MaxMicroTimeResolution);
-    if ~exist('TimeWindow', 'var') || isempty(TimeWindow)
-        TimeWindow = double(FCSHeader.MicroTimeRange); % ns
+    if ~exist('MicroTimeRange', 'var') || isempty(MicroTimeRange)
+        MicroTimeRange = double(FCSHeader.MicroTimeRange); % ns
     end
     if strcmpi(EarlyPulseOrLatePulse, 'early')
-        HalfedTimeWindow = 1 : (ADCResolution / 2);
+        TimeWindow = 1 : (ADCResolution / 2);
     elseif strcmpi(EarlyPulseOrLatePulse, 'late')
-        HalfedTimeWindow = (ADCResolution / 2 + 1) : ADCResolution;
+        TimeWindow = (ADCResolution / 2 + 1) : ADCResolution;
+    elseif strcmpi(EarlyPulseOrLatePulse, 'whole') || ...
+            strcmpi(EarlyPulseOrLatePulse, 'entire') || ...
+            strcmpi(EarlyPulseOrLatePulse, 'all')
+        TimeWindow = 1 : ADCResolution;
     else
-        error('Please specify between the early pulse or the late pulse.');
+        error('Please specify the microtime window.');
     end
     
     %% Calculate IRF
     PhotonCounts = histcounts(Trace{1}.MicroTimeData, ...
-        0 : (TimeWindow / ADCResolution) : TimeWindow);
+        0 : (MicroTimeRange / ADCResolution) : MicroTimeRange);
 %     NormalizedPhotonCounts = PhotonCounts / sum(PhotonCounts);
-%     IRFProb = NormalizedPhotonCounts(HalfedTimeWindow);
-    IRFProb = PhotonCounts(HalfedTimeWindow) / ...
-        sum(PhotonCounts(HalfedTimeWindow));
+%     IRFProb = NormalizedPhotonCounts(TimeWindow);
+    IRFProb = PhotonCounts(TimeWindow) / ...
+        sum(PhotonCounts(TimeWindow));
     
     %% Save
     save([Filename, '.mat'], 'IRFProb');

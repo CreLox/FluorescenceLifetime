413af337d62bbe58e6cc4fd1fa1f5aad13904061
Chu Chen <CreLox@users.noreply.github.com>
2020-01-21 12:36:57 -0500

    Add files via upload

--- drawMasks.m
+++ drawMasks.m
@@ -1,7 +1,15 @@
-% SelectedPixelIndices = drawMasks(ImageMatrix)
-function SelectedPixelIndices = drawMasks(ImageMatrix)
-    h = figure;
-    imshow(squeeze(ImageMatrix), []);
+% SelectedPixelIndices = drawMasks(Image)
+function SelectedPixelIndices = drawMasks(Image)
+    if isnumeric(Image)
+        h = figure;
+        imshow(squeeze(Image), []);
+        Mask = false(size(squeeze(Image)));
+    elseif isa(Image, 'matlab.ui.Figure')
+        h = figure(Image);
+        Mask = false(size(Image.UserData));
+    else
+        error('The input has to be either a matrix or a figure handle');
+    end
     
     Number = 0;
     doDraw = 'y';
@@ -13,11 +21,13 @@ function SelectedPixelIndices = drawMasks(ImageMatrix)
         doDraw = input('Add one more? ', 's');
     end
     
-    Mask = false(size(squeeze(ImageMatrix)));
+    
     for i = 1 : Number
         Mask = Mask | NewArea{i}.createMask;
     end
     SelectedPixelIndices = find(Mask);
+    SelectedPixelIndices = reshape(SelectedPixelIndices, 1, ...
+        numel(SelectedPixelIndices));
     
     close(h);
 end

1c0c5d195b944aedd46c746fe022cd81dca730df
Chu Chen <chenchu@umich.edu>
2021-09-02 14:47:59 -0400

    Add files via upload

--- Workflows/PhasorIntensityFiltersFLIMFitting.m
+++ Workflows/PhasorIntensityFiltersFLIMFitting.m
@@ -9,20 +9,21 @@ axis on;
 
 %% Step 2
 f2 = figure(2);
-PhotonCountFilter = [165, 312]; % <- Change this accordingly
+PhotonCountFilter = [145, 334]; % <- Change this accordingly
 imshowThresholding(Image, PhotonCountFilter, f2);
 axis on;
 
 %% Step 3: phasor filtering
+% Plot phasor- and intensity-filtered Image
 close(f2);
-load('ludox_488nm_ch2.mat'); % <- Change this accordingly
+load('20210825_mirror_irf.mat'); % <- Change this accordingly
 [PhasorG, PhasorS, PixelIndices] = calculatePhasor(FlimDataPre, ...
     PhotonCountFilter, Omega, IRFTransform);
 Outlier = (PhasorS < (PhasorG - 0.2));
 OutlierIdx = PixelIndices(Outlier);
 Image(OutlierIdx) = 0;
 f2 = figure(2);
-h = imshowThresholding(Image, PhotonCountFilter, f2); % Phasor- and intensity-filtered Image
+h = imshowThresholding(Image, PhotonCountFilter, f2);
 h.UserData = Image;
 axis on;
 
@@ -33,7 +34,10 @@ OutlierMapping(OutlierIdx) = true;
 imshow(OutlierMapping);
 axis on;
 
-% The wait is long - play Handel's "Hallelujah" when this step finishes
+% The wait is usually long - thank god when it is finally finished!
+% (play the sound effect of the Holy Hand Grenade, a.k.a. Handel's
+% "Hallelujah", when this step finishes)
+% https://en.wikipedia.org/wiki/Rabbit_of_Caerbannog
 load handel.mat; soundsc(y(1000:16500)); clear Fs y;
 
 %% Step 4: extra pixel-filtering
@@ -47,10 +51,13 @@ FittingOption = 'Fitting2S';
 Results = fitFLIM(FlimData, IRFProb, FittingOption, 1)
 
 MedianEventCountPerPixel = eventCountPer('Pixel', FlimData, @(x) median(x))
-A0WeightedMean = harmmeanWeighted([Results.OptimX(2), Results.OptimX(4)], [Results.OptimX(1), Results.OptimX(3)])
-TotalIntensityWeightedMean = Results.OptimX(1) * Results.OptimX(2) + Results.OptimX(3) * Results.OptimX(4)
+A0WeightedMean = harmmeanWeighted([Results.OptimX(2), Results.OptimX(4)], ...
+    [Results.OptimX(1), Results.OptimX(3)])
+TotalIntensityWeightedMean = Results.OptimX(1) * Results.OptimX(2) + ...
+    Results.OptimX(3) * Results.OptimX(4)
 
 [~, FileName, ~] = fileparts(FileName);
 figure(1); savefig(sprintf('%s.fig', FileName));
 figure(2); savefig(sprintf('%s_Residuals.fig', FileName));
-save(FileName, 'FlimData', 'FittingOption', 'IRFProb', 'OutlierIdx', 'PhotonCountFilter', 'Results', '-v7.3');
+save(FileName, 'FlimData', 'FittingOption', 'IRFProb', 'OutlierIdx', ...
+    'PhotonCountFilter', 'Results', '-v7.3');
